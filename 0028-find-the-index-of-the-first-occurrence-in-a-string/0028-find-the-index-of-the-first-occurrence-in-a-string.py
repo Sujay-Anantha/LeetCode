e548b0e9a2d3f8ccdd344531388e869b0cbf24e8
Sujay Anantha <133889166+Sujay-Anantha@users.noreply.github.com>
2024-06-07 11:09:09 -0400

    Time: 40 ms (19.58%), Space: 16.4 MB (83.34%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,10 +1,50 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        print(len(haystack))
-        for i in range(len(haystack)):
-            print(haystack[i:i+len(needle)])
-            if haystack[i:i+len(needle)] == needle:
-                return i
+        # Edge case: if needle is empty, return 0
+        if not needle:
+            return 0
+        
+        # Generate the longest prefix suffix (LPS) array for the needle
+        def computeLPSArray(needle):
+            m = len(needle)
+            lps = [0] * m
+            length = 0  # Length of the previous longest prefix suffix
+            i = 1
+
+            while i < m:
+                if needle[i] == needle[length]:
+                    length += 1
+                    lps[i] = length
+                    i += 1
+                else:
+                    if length != 0:
+                        length = lps[length - 1]
+                    else:
+                        lps[i] = 0
+                        i += 1
+            return lps
+        
+        # KMP algorithm to search for needle in haystack
+        n = len(haystack)
+        m = len(needle)
+        lps = computeLPSArray(needle)
+        
+        i = 0  # Index for haystack
+        j = 0  # Index for needle
+        
+        while i < n:
+            if needle[j] == haystack[i]:
+                i += 1
+                j += 1
             
+            if j == m:
+                return i - j
+            elif i < n and needle[j] != haystack[i]:
+                if j != 0:
+                    j = lps[j - 1]
+                else:
+                    i += 1
+        
         return -1
+
         
