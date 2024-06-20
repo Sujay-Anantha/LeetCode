11b77bed63513ac8eb384fdffab464fe54c44848
Sujay Anantha <133889166+Sujay-Anantha@users.noreply.github.com>
2024-06-20 13:13:12 -0400

    Time: 353 ms (86.38%), Space: 17.1 MB (25.76%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,27 +1,18 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        if n == 0:
-            return len(tasks)
+        # frequencies of the tasks
+        frequencies = [0] * 26
+        for t in tasks:
+            frequencies[ord(t) - ord('A')] += 1
         
-        # Count the frequency of each task
-        task_counts = Counter(tasks)
-        # Create a max heap based on the task counts
-        max_heap = [-count for count in task_counts.values()]
-        heapq.heapify(max_heap)
+        frequencies.sort()
+
+        # max frequency
+        f_max = frequencies.pop()
+        idle_time = (f_max - 1) * n
         
-        time = 0
-        # Queue to keep track of the cooldown period for tasks
-        cooldown_queue = deque()
-        
-        while max_heap or cooldown_queue:
-            time += 1
-            
-            if max_heap:
-                count = 1 + heapq.heappop(max_heap)  # Pop the most frequent task
-                if count:  # If there are more instances of this task, put it in the cooldown queue
-                    cooldown_queue.append((count, time + n))
-            
-            if cooldown_queue and cooldown_queue[0][1] == time:
-                heapq.heappush(max_heap, cooldown_queue.popleft()[0])
-        
-        return time
+        while frequencies and idle_time > 0:
+            idle_time -= min(f_max - 1, frequencies.pop())
+        idle_time = max(0, idle_time)
+
+        return idle_time + len(tasks)
