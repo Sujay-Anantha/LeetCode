69b62836132ea147e8b634e6d077441599103c11
Sujay Anantha <133889166+Sujay-Anantha@users.noreply.github.com>
2024-09-17 09:42:16 -0400

    Time: 37 ms (43.96%), Space: 16.5 MB (31.00%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,13 +1,18 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        m,n = len(word1) , len(word2)
-        merged = ""
-        for i in range (min(m,n)):
-            merged += word1[i]+word2[i]
+        m, n = len(word1), len(word2)
+        merged = []
         
+        # Merge the characters alternately
+        for i in range(min(m, n)):
+            merged.append(word1[i])
+            merged.append(word2[i])
+        
+        # Add the remaining part of the longer string
         if m > n:
-            merged = merged + word1[n:]
-        if n>m:
-            merged = merged + word2[m:]
+            merged.append(word1[n:])
+        elif n > m:
+            merged.append(word2[m:])
         
-        return merged
+        # Join the list into a single string
+        return ''.join(merged)
