f36e2dd1c83cdf6adf48f414deda668084fa9fdf
Sujay Anantha <133889166+Sujay-Anantha@users.noreply.github.com>
2024-09-22 20:06:38 -0400

    Time: 31 ms (79.24%), Space: 16.6 MB (19.62%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,9 +1,15 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        if s == goal:
-            return True
-        for i in range(1,len(s)):
-            rearrange = s[i:]+s[0:i]
-            if rearrange == goal:
-                return True
-        return False
+        # if s == goal:
+        #     return True
+        # for i in range(1,len(s)):
+        #     rearrange = s[i:]+s[0:i]
+        #     if rearrange == goal:
+        #         return True
+        # return False
+        
+        # Check if the lengths of s and goal are the same
+        if len(s) != len(goal):
+            return False
+        # Check if goal is a substring of s concatenated with itself
+        return goal in s + s
