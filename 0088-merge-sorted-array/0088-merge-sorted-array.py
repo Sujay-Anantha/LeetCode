b3336b26ad7f449938489ac45748afe0da59f450
Sujay Anantha <133889166+Sujay-Anantha@users.noreply.github.com>
2024-09-23 10:56:56 -0400

    Time: 41 ms (47.24%), Space: 16.4 MB (89.08%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,23 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # Initialize pointers
-        i = m + n - 1  # Pointer for the last position in nums1
-        p1 = m - 1     # Pointer for the last element in the initial part of nums1
-        p2 = n - 1     # Pointer for the last element in nums2
 
-        # Merge nums2 into nums1 starting from the end
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] > nums2[p2]:
-                nums1[i] = nums1[p1]
+        # Set p1 and p2 to point to the end of their respective arrays.
+        p1 = m - 1
+        p2 = n - 1
+
+        # And move p backward through the array, each time writing
+        # the smallest value pointed at by p1 or p2.
+        for p in range(n + m - 1, -1, -1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
                 p1 -= 1
             else:
-                nums1[i] = nums2[p2]
+                nums1[p] = nums2[p2]
                 p2 -= 1
-            i -= 1
-
-        # If there are elements left in nums2, copy them
-        while p2 >= 0:
-            nums1[i] = nums2[p2]
-            i -= 1
-            p2 -= 1
